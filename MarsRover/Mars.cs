b12fa26f409400eb4543fb936ffd6ab3a2facbbf
leerling <leerling@IBP-12>
2019-01-29 12:00:37 +0100

    Spawnen rotsen

--- MarsRover/Mars.cs
+++ MarsRover/Mars.cs
@@ -10,6 +10,13 @@ namespace MarsRover
     {
         int grootteX = 40;
         int grootteY = 20;
+        bool[,] arrayrotsen = new bool[40,20];
+
+        public Mars()
+        {
+            RotsenGenereren();
+            RotsenTonen();
+        }
 
         //Maakt een omgeving waarin je kan bewegen (in de console)
         public void toonMars()
@@ -42,6 +49,38 @@ namespace MarsRover
                 Console.SetCursorPosition(i, grootteY);
                 Console.Write("═");
             }
+
+
+        }
+
+        public void RotsenTonen() // YANNICK: Spawn rotsen op random locatie binnen gebied //This is torture
+        {
+            for (int i = 0; i < arrayrotsen.GetLength(0); i++)
+            {
+                for (int l = 0; l < arrayrotsen.GetLength(1); l++)
+                {
+                    if (arrayrotsen[i,l] == true)
+                    {
+                        char charrots = '£';
+                        Console.SetCursorPosition(i, l);
+                        Console.WriteLine(charrots);
+                    }
+                    
+
+                }
+            }
+        }
+
+        public void RotsenGenereren()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                arrayrotsen[random.Next(1, 39),random.Next(1, 19)] = true;
+            }
+           
+
         }
     }
 }
